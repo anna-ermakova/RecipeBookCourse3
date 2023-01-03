a4281064a97425eb5c0a51b5185d4bdd910e6367
anna-ermakova <korovanna8@mail.ru>
2023-01-03 14:30:33 +0300

    начальный коммит

---
+++ src/main/java/pro/sky/recipebookcourse3/RecipeBookCourse3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.recipebookcourse3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookCourse3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBookCourse3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/recipebookcourse3/RecipeBookCourse3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipebookcourse3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookCourse3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
